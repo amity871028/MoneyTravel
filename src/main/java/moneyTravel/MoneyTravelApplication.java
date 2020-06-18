a6eeccf22ff84887ecb0003813ee0849b3c35c09
amity871028 <amity3744039@gmail.com>
2020-06-18 15:54:05 +0800

    init project

---
+++ src/main/java/moneyTravel/MoneyTravelApplication.java
@@ -0,0 +1,13 @@
+package moneyTravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moneyTravel/MoneyTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package moneyTravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
